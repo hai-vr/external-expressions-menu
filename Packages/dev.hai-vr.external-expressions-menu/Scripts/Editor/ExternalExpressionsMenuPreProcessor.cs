6c819b1373f08dbe2c5f8131e29b8094a5afc325
Haï~ <hai-vr@users.noreply.github.com>
2024-08-27 08:44:18 +0200

    If NDMF is not installed, use IVRCSDKPreprocessAvatarCallback: - If NDMF is not installed, use IVRCSDKPreprocessAvatarCallback instead. - NDMF is no longer required.

---
+++ Packages/dev.hai-vr.external-expressions-menu/Scripts/Editor/ExternalExpressionsMenuPreProcessor.cs
@@ -0,0 +1,21 @@
+﻿#if !EEM_NDMF_IS_INSTALLED
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+namespace ExternalExpresssionsMenu.Editor
+{
+    public class ExternalExpressionsMenuPreProcessor : IVRCSDKPreprocessAvatarCallback
+    {
+        // Arbitrary number. Run as late as possible. We don't rely on IEditorOnly stuff so there's no concerns about -1024,
+        // as long as this runs after other non-destructive processors.
+        public int callbackOrder => 12000;
+        public bool OnPreprocessAvatar(GameObject avatarGameObject)
+        {
+            var process = new ExternalExpresssionsMenuProcess();
+            process.ExtractMenu(avatarGameObject.transform, avatarGameObject.GetComponent<VRCAvatarDescriptor>());
+            return true;
+        }
+    }
+}
+#endif
